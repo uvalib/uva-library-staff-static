07d27b692c87c054c8e787be8dd3877a62743020
ssw3r <starrie@virginia.edu>
2015-09-03 15:14:32 -0400

    Initial commit of files
    This is the first commit

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+
+    $(".dropdown-button").dropdown();
+      $('.slider').slider({full_width: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
